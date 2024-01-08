747d60c6aa23ed36ebd600be275842939133b221
Nacho <nacho.conso@gmail.com>
2024-01-08 19:26:21 -0300

    add modify shell script to prevent changing directory

---
+++ ci/set_envs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# This script reads the current ENV environment variable value and injects
+# the corresponding parameter values to the template.yaml before `sam deploy`
+
+script_dir=$(dirname "$(realpath "$0")")
+
+if [ -z "$ENV" ]; then
+  echo "Error: ENV environment variable not set"
+  exit 1
+fi
+
+env_file=$(find "$script_dir" -maxdepth 1 -type f -name ".env.$ENV")
+
+if [ -z "$env_file" ]; then
+  echo "Error: .env.$ENV not found in the script's directory"
+  exit 1
+fi
+
+parameter_overrides="--parameter-overrides"
+
+while IFS='=' read -r key value || [[ -n "$key" ]]; do
+  parameter_overrides+=" $key=$value"
+done < "$env_file"
+
+echo "Parameter overrides for '$ENV' environment"
+echo "$parameter_overrides"
