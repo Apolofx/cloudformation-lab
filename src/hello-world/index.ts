1b7675ff16732cb0ffde03b4247687d7fa564ceb
Nacho <nacho.conso@gmail.com>
2024-01-08 18:28:16 -0300

    use esbuild to build lambda code

--- src/hello-world/index.ts
+++ src/hello-world/index.ts
@@ -1,5 +1,5 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
-
+import axios from "axios";
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -13,12 +13,17 @@ import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
  *
  */
 
+// Test this function running: `sam local invoke --event events/event.json HelloWorldFunction`
+
 export const handler = async (event: APIGatewayProxyEventV2) => {
-  console.log(event);
+  const test_payload = await axios
+    .get("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.data);
   const response: APIGatewayProxyResultV2 = {
     statusCode: 200,
     body: JSON.stringify({
       message: "hello storydots!",
+      body: test_payload,
     }),
   };
 
